2e90e079b2e44162687fbcfab25eb02395d62172
Namdak Tonpa <maxim@synrc.com>
2017-05-29 23:00:04 +0300

    more async review

--- src/index.erl
+++ src/index.erl
@@ -6,15 +6,28 @@
 
 event(init) ->
     nitro:wire("nodes="++nitro:to_list(length(n2o:ring()))++";"),
-    #cx{session=ClientId} = get(context),
+    #cx{session=ClientId,node=Node} = get(context),
     Room = n2o:cache(room),
     nitro:update(logout,  #button { id=logout,  body="Logout "  ++ n2o:user(),       postback=logout }),
     nitro:update(send,    #button { id=send,    body="Chat",       source=[message], postback=chat   }),
     nitro:update(heading, #b      { id=heading, body="Review: " ++ Room}),
     nitro:update(upload,  #upload { id=upload   }),
     nitro:wire("mqtt.subscribe('room/"++Room++"',subscribeOptions);"),
+    [N] = binary_to_list(Node),
+    Topic = iolist_to_binary(["events/",nitro:to_list(N),"/index/anon/",ClientId]),
+    io:format("Topic: ~tp~n",[Topic]),
+    n2o:send_reply(ClientId, 2,
+        Topic,
+        term_to_binary(#client{id=Room,data=list}));
+
+% proto of roster message
+
+event(#client{id=Room,data=list}) ->
+    io:format("ROSTER~n"),
     [ event(#client{data={E#entry.from,E#entry.media}})
-      || E <- lists:reverse(kvs:entries(kvs:get(feed,{room,Room}),entry,10)) ];
+      || E <- lists:reverse(kvs:entries(kvs:get(feed,{room,Room}),entry,30)) ];
+
+% proto of send message
 
 event(chat) ->
     User    = n2o:user(),
@@ -28,8 +41,10 @@ event(chat) ->
     event(#client{data={User,Message}}),
     Actions = iolist_to_binary(n2o_nitro:render_actions(n2o:actions())),
     M = term_to_binary({io,Actions,<<>>}),
-    
+
     n2o:send_reply(ClientId, 2, iolist_to_binary([<<"room/">>,Room]), M);
+
+% proto of UI update
 
 event(#client{data={User,Message}}) ->
      nitro:wire(#jq{target=message,method=[focus,select]}),
@@ -47,6 +62,7 @@ event(Event)  -> io:format("Event: ~p", [Event]).
 
 main() -> [].
 
-message_view(Color,User,Message) -> 
+message_view(Color,User,Message) ->
    iolist_to_binary(["<table width=500 cellpadding=20 cellspacing=3><tr><td bgcolor=",Color,
        " style='color:white;'><b>",User,"</b>: ",Message,"</td></tr></table>"]).
+
