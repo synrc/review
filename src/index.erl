b12900d1938425afdc877a74d1bc58b58e06311a
Namdak Tonpa <maxim@synrc.com>
2017-06-01 01:45:57 +0300

    fix login

--- src/index.erl
+++ src/index.erl
@@ -55,8 +55,11 @@ event(#ftp{sid=Sid,filename=Filename,status={event,stop}}=Data) ->
     nitro:render(#link{href=iolist_to_binary(["/spa/",Sid,"/",nitro_conv:url_encode(Name)]),body=Name})),
     event(chat);
 
-event(logout) -> nitro:redirect(n2o:session(room));
+event(logout) -> nitro:redirect(fix(n2o:session(room)));
 event(Event)  -> io:format("Event: ~p", [Event]).
+
+fix([]) -> "login";
+fix(A) -> A.
 
 main() -> [].
 
