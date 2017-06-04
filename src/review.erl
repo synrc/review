1a5c1d36ff66c9a93e195e3a9bfcc99c64b834cd
Namdak Tonpa <maxim@synrc.com>
2017-06-04 15:57:19 +0300

    review

--- src/review.erl
+++ src/review.erl
@@ -16,7 +16,7 @@ init([])   -> application:set_env(n2o,session,n2o),
               {ok, {{one_for_one, 5, 10}, [spec()]}}.
 spec()     -> ranch:child_spec(http, 100, ranch_tcp, port(), cowboy_protocol, env()).
 env()      -> [ { env, [ { dispatch, points() } ] } ].
-static()   ->   { dir, "apps/review/priv", mime() }.
+static()   ->   { dir, "apps/review", mime() }.
 static2()   ->   { dir, "deps/spa/priv", mime() }.
 n2o()      ->   { dir, "deps/n2o/priv",    mime() }.
 mime()     -> [ { mimetypes, cow_mimetypes, all   } ].
