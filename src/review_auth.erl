a699ee5124e9fca6913fd99785e69faba0a61057
Namdak Tonpa <maxim@synrc.com>
2017-11-24 15:13:30 +0200

    review_auth test proto

--- src/review_auth.erl
+++ src/review_auth.erl
@@ -9,4 +9,17 @@ info({init, <<>>}, Req, State = #cx{session = Session}) ->
     io:format("Token Saved: ~p~n",[Token]),
     n2o_nitro:info({init, Token}, Req, State);
 
+% test protocol
+
+info({load, <<>>}, Req, State = #cx{session = Session}) ->
+    #cx{params = ClientId} = get(context),
+    M = "list of previous messages",
+    n2o:send_reply(ClientId, 2, iolist_to_binary([<<"actions/1/index/">>,ClientId]), M),
+    {reply, {binary, term_to_binary(<<>>)}, Req, State};
+
+info({message, Text} = M, Req, State = #cx{session = Session}) ->
+    #cx{params = ClientId} = get(context),
+    n2o:send_reply(ClientId, 2, <<"room/global">>, M),
+    {reply, {binary, term_to_binary(<<>>)}, Req, State};
+
 info(Message,Req,State) -> {unknown,Message,Req,State}.
