2e08cfd6ac3ce69114a673fb20eb7f1a1570a313
Namdak Tonpa <maxim@synrc.com>
2018-11-04 20:15:22 +0200

    remove child_spec/4

--- src/review.erl
+++ src/review.erl
@@ -7,25 +7,6 @@
 
 -compile(export_all).
 
--ifndef(RECBUF_SIZE).
--define(RECBUF_SIZE, 8192).
--endif.
-
--ifndef(SOCKET_OPTS). 
--define(SOCKET_OPTS, [
-    binary,
-    {reuseaddr, true},
-    {packet, raw},
-    {backlog, 1024},
-    {recbuf, ?RECBUF_SIZE},
-    {send_timeout, 15000},
-    {send_timeout_close, true},
-    {exit_on_close, false},
-    {nodelay, false}
-]).
--endif.
-
-
 main(A)    -> mad:main(A).
 start()    -> start(normal,[]).
 start(_,_) -> supervisor:start_link({local,review},review,[]).
@@ -35,24 +16,17 @@ init([])   -> application:set_env(n2o,session,n2o_session),
               application:set_env(n2o,fixpath,{review,fix2}),
               application:set_env(kvs,dba,store_mnesia),
               emqttd_access_control:register_mod(auth, n2o_auth, [[]], 9998),
-              
+
               {ok, {{one_for_one, 5, 10}, [spec()]}}.
 
-spec()     -> 
+spec()     ->
     Acceptors = application:get_env(?MODULE, acceptors, 4),
     MaxClients = application:get_env(?MODULE, max_clients, 512),
     Options = [{max_clients, MaxClients}, {acceptors, Acceptors}],
     Args = [{?MODULE, handle, [docroot()]}],
     Protocol = application:get_env(?MODULE, protocol, http),
     Port = application:get_env(?MODULE, port, 8000),
-    child_spec(Protocol, Port, Options, Args)
-.
-
-child_spec(Protocol, ListenOn, Options, Args) ->
-        SockOpts = merge_opts(?SOCKET_OPTS, proplists:get_value(sockopts, Options, [])),
-        MFArgs = {mochiweb_http, start_link, Args},
-        esockd_sup:child_spec(Protocol, ListenOn, SockOpts, MFArgs)
-.
+    mochiweb:child_spec(Protocol, Port, Options, Args).
 
 merge_opts(Defaults, Options) ->
     lists:foldl(
@@ -74,8 +48,7 @@ dispatch(Path) ->
     case Path of
         [] -> application:get_env(?MODULE, main_page, "index.htm");
         _ -> Path
-    end
-.
+    end.
 
 handle(Req, DocRoot) ->
     io:format("~s ~s~n", [Req:get(method), Req:get(path)]),
@@ -98,16 +71,14 @@ handle(Req, DocRoot) ->
         error_logger:error_report(Report),
         Req:respond({500, [{"Content-Type", "text/plain"}],
                     "request failed, sorry\n"})
-    end
-.
+    end.
 
 docroot() ->
     {file, Here} = code:is_loaded(?MODULE),
     Dir = filename:dirname(filename:dirname(Here)),
     StaticsRoot = application:get_env(?MODULE, "statics_root", "priv/www"),
-    filename:join([Dir, StaticsRoot])
-.
-            
+    filename:join([Dir, StaticsRoot]).
+
 
 % spec()     -> ranch:child_spec(http, 100, ranch_tcp, port(), cowboy_protocol, env()).
 % env()      -> [ { env, [ { dispatch, points() } ] } ].
