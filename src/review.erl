1d443654b3659202ad7ae28801c4c7b0946e0dc1
Namdak Tonpa <maxim@synrc.com>
2018-11-15 06:54:52 +0200

    seq=10

--- src/review.erl
+++ src/review.erl
@@ -7,7 +7,7 @@
 main(A)    -> mad:main(A).
 init([])   -> {ok, {{one_for_one, 5, 10}, [spec()]}}.
 start()    -> start(normal,[]).
-start(_,_) -> emqttd_access_control:register_mod(auth, n2o_auth, [[]], 9998),
+start(_,_) -> emqttd_access_control:register_mod(auth, n2o_auth, [[]], 10),
               supervisor:start_link({local,review},review,[]).
 stop(_)    -> ok.
 spec()     ->
