4e8b76060d3e6c44c7c0994788d71e749633c21c
tonpa <maxim@synrc.com>
2020-10-12 12:08:54 +0000

    n2o_sed

--- src/n2o_sed.erl
+++ src/n2o_sed.erl
@@ -2,14 +2,20 @@
 -description('MIA:CRM protocol').
 -compile({parse_transform, bert_javascript}).
 -include_lib("review/include/erp/catalogs/employee.hrl").
+-include_lib("review/include/erp/catalogs/organization.hrl").
+-include_lib("review/include/erp/catalogs/branch.hrl").
 -export([info/3]).
 
 info(#'Employee'{}, Req, State) ->
     io:format("ERP.UNO Employee object sent ~n"),
     {reply, {binary, term_to_binary({reply,1})}, Req, State};
 
-info({'Employee',1} = X, Req, State) ->
-    io:format("Employee tuple ~p~n",[X]),
-    {reply, {bert, {reply,1}}, Req, State};
+info(#'Organization'{}, Req, State) ->
+    io:format("ERP.UNO Organization object sent ~n"),
+    {reply, {binary, term_to_binary({reply,1})}, Req, State};
+
+info(#'Branch'{}, Req, State) ->
+    io:format("ERP.UNO Branch object sent ~n"),
+    {reply, {binary, term_to_binary({reply,1})}, Req, State};
 
 info(Message, Req, State) -> {unknown,Message, Req, State}.
