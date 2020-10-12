1f83f6ba685da43765321888742a26af3eb7ea02
Andrii Zadorozhnii <doxtop@synrc.com>
2020-10-12 20:23:38 +0300

    declare cowboy routes

--- lib/review.ex
+++ lib/review.ex
@@ -3,7 +3,20 @@ defmodule Review.App do
    use Supervisor
    require KVS
 
-   def env(),  do: %{env: %{dispatch: :n2o_cowboy.points()}}
+   def mime(), do: [{:mimetypes, :cow_mimetypes, :all}]
+   def points() do
+    :cowboy_router.compile([
+      {:_,
+       [
+         {'/ws/[...]',    :n2o_cowboy, []},
+         {'/n2o/[...]',   :cowboy_static, {:dir, :code.priv_dir(:n2o), mime()}},
+         {'/nitro/[...]', :cowboy_static, {:dir, :code.priv_dir(:nitro) ++ "/js",  mime()}},
+         {'/app/[...]',   :cowboy_static, {:dir, :code.priv_dir(:review) ++ "/static", mime()}}
+       ]}
+    ])
+   end
+
+   def env(),  do: %{env: %{dispatch: points()}}
    def trans(),do: [{:port, :application.get_env(:n2o, :port, 8000)}]
    def spec(), do: :ranch.child_spec(:http, 100, :ranch_tcp, trans(), :cowboy_clear, env())
 
