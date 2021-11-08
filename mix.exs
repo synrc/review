566d99b0206d73fe7f5cbe67f17af62a01a084db
Namdak Tonpa <maxim@synrc.com>
2021-11-08 13:15:42 +0200

    clean up mix.hex, @doxtop please take a look

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Review.MixProject do
  def project do
     [
       app: :review,
-      version: "1.2.0",
-      description: "Review",
+      version: "4.11.0",
+      description: "REVIEW TT Sample N2O/MQTT Application",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       erlc_paths: ["src"],
@@ -30,17 +30,17 @@ defmodule Review.MixProject do
 
  def deps do
   [
-    {:syn, github: "ostinelli/syn", ref: "2.1.1", override: true},  # {:syn, "~> 2.1.1", override: true},
-    {:kvs, github: "synrc/kvs", ref: "7.9.1", override: true},      # {:kvs, "~> 7.9.1", override: true},
-    {:getopt, github: "xio/getopt", ref: "master", override: true},
     {:cowlib, "~> 2.9.1", override: true},
     {:cowboy, "~> 2.8.0", override: true},
     {:ranch,  "~> 1.7.1", override: true},
-    {:emqx,  github: "xio/emqx",    ref: "erp.uno"},
-    {:emqtt, github: "xio/emqtt",   ref: "master"},
-    {:rpc,   github: "synrc/rpc",   ref: "3.1"},
-    {:n2o,   github: "synrc/n2o",   ref: "master", override: true},    # {:n2o, "~> 7.10.0"}
-    {:nitro, github: "synrc/nitro", ref: "master", override: true}, # {:nitro, "~> 5.9.1", override: true}
+    {:n2o,    "~> 8.8.1", override: true},
+    {:syn,    "~> 2.1.1", override: true},
+    {:kvs,    "~> 8.10.4", override: true},
+    {:rpc,    "~> 3.1.1", override: true},
+    {:nitro,  "~> 6.11.6", override: true},
+    {:getopt, github: "xio/getopt", ref: "master", override: true},
+    {:emqx,   github: "xio/emqx",   ref: "erp.uno"},
+    {:emqtt,  github: "xio/emqtt",  ref: "master"},
   ]
  end
 end
